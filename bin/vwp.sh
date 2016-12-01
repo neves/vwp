943300f86dbec77f36670e1208bd07ee9678294a
Marcos Neves <marcos.neves@gmail.com>
2016-12-01 15:05:52 -0200

    Use local wpc bin

--- bin/vwp.sh
+++ bin/vwp.sh
@@ -3,4 +3,4 @@
 # https://gist.github.com/branneman/8048520#4-the-environment
 
 parent=$(node -e "console.log(require('path').resolve('$0', '../..'))")
-NODE_PATH="$parent/node_modules" wpc $@ --config "$parent/webpackfile.js"
+NODE_PATH="$parent/node_modules" $parent/node_modules/.bin/wpc $@ --config "$parent/webpackfile.js"
