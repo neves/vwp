2ffef1a5f2d465a1943ea95ec02f2cdb21d05672
Marcos Neves <marcos.neves@gmail.com>
2016-12-01 16:46:27 -0200

    Fix global node_modules path

--- bin/vwp.sh
+++ bin/vwp.sh
@@ -3,4 +3,5 @@
 # https://gist.github.com/branneman/8048520#4-the-environment
 
 parent=$(node -e "console.log(require('path').resolve(require('fs').realpathSync('$0'), '../..'))")
-NODE_PATH="$parent/node_modules" $parent/node_modules/.bin/wpc $@ --config "$parent/webpackfile.js"
+global=$(dirname $parent)
+NODE_PATH="$global" $parent/node_modules/.bin/wpc $@ --config "$parent/webpackfile.js"
