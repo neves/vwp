4c1f224baeea2af12fc447712dc1262f47dfdbd7
Marcos Neves <marcos.neves@gmail.com>
2016-12-01 15:17:30 -0200

    Fix link resolving to wpc

--- bin/vwp.sh
+++ bin/vwp.sh
@@ -2,5 +2,5 @@
 # add local and global node_modules to the path
 # https://gist.github.com/branneman/8048520#4-the-environment
 
-parent=$(node -e "console.log(require('path').resolve('$0', '../..'))")
+parent=$(node -e "console.log(require('fs').realpathSync(require('path').resolve('$0', '../..')))")
 NODE_PATH="$parent/node_modules" $parent/node_modules/.bin/wpc $@ --config "$parent/webpackfile.js"
