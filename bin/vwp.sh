0a8e09c763dd67c25c21d51a5462b7f054ddd39d
Marcos Neves <marcos.neves@gmail.com>
2016-12-02 14:34:04 -0200

    Refactor bin

--- bin/vwp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# add local and global node_modules to the path
-# https://gist.github.com/branneman/8048520#4-the-environment
-
-parent=$(node -e "console.log(require('path').resolve(require('fs').realpathSync('$0'), '../..'))")
-global=$(dirname $parent)
-NODE_PATH="$global" $parent/node_modules/.bin/wpc $@ --config "$parent/webpackfile.js"
--- webpackfile.js
+++ webpackfile.js
@@ -1,50 +1,47 @@
 const merge = require('wpc/merge')
 
-var config = merge(
-  require('wpc/defaults'),
-{
-  env: {
-    root: process.cwd(),
-    src: process.cwd() + '/src'
-  }
+module.exports = env => {
+  var root = process.cwd()
+  var config = merge(
+    require('wpc/defaults'),
+    ({context}) => ({
+      env: {
+        root: root,
+        src: root + '/src'
+      },
+      entry: root + '/src/index.js',
+      output: {
+        path: root + '/dist',
+        filename: '[name].js'
+      },
+      devServer: {
+        host: '0.0.0.0',
+        noInfo: true
+      }
+    }),
+    require('wpc/resolve-src'),
+    require('wpc/global-modules'),
+    require('wpc/global-loaders'),
+    require('wpc/define-env'),
+    require('wpc/define-version'),
+    require('wpc/css'),
+    require('wpc/coffee'),
+
+    require('wpc/babel'),
+    require('wpc/babel-es2015'),
+    require('wpc/babel-stage3'),
+
+    require('wpc/vue'),
+    require('wpc/vue-babel'),
+    require('wpc/vue-pug'),
+    require('wpc/vue-scss'),
+    require('wpc/vue-sass'),
+    require('wpc/vue-stylus'),
+
+    require('wpc/html'),
+    require('wpc/dump-conf'),
+    require('wpc/delete-properties')
+  )
+
+  return config
 }
-,
-{
-  context: process.cwd(),
-  entry: process.cwd() + '/src/index.js',
-  output: {
-    path: process.cwd() + '/dist',
-    filename: '[name].js'
-  },
-  devServer: {
-    host: '0.0.0.0',
-    noInfo: true
-  },
-
-},
-  require('wpc/resolve-src'),
-  require('wpc/global-node-modules'),
-  require('wpc/define-env'),
-  require('wpc/define-version'),
-  require('wpc/css'),
-  require('wpc/coffee'),
-
-  require('wpc/babel'),
-  require('wpc/babel-es2015'),
-  require('wpc/babel-stage3'),
-
-  require('wpc/vue'),
-  require('wpc/vue-babel'),
-  require('wpc/vue-pug'),
-  require('wpc/vue-scss'),
-  require('wpc/vue-sass'),
-  require('wpc/vue-stylus'),
-
-  require('wpc/html')
-)
-
-delete config.env
-// console.log(new Set(config.packages))
-delete config.packages
-
-module.exports = config
