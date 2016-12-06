fe0d5dad9159c7cc4c02f7ca914bf9b86dc3e535
Marcos Neves <marcos.neves@gmail.com>
2016-12-06 15:14:14 -0200

    Update wpc

--- webpackfile.js
+++ webpackfile.js
@@ -1,52 +1,49 @@
-const merge = require('wpc/merge')
+merge = require('wpc/merge')
+const Path = require('path')
 
-module.exports = env => merge(
-    require('wpc/defaults'),
-    ({context}) => ({
-      env: {
-        debug: process.env.DEBUG || ['--debug', '-d', '--verbose'].some(e => process.argv.includes(e)),
-        src: context + '/src'
-      },
-      entry: context + '/src/index.js',
-      output: {
-        path: context + '/dist'
-      },
-      devServer: {
-        host: '0.0.0.0',
-        noInfo: true
-      }
-    }),
+module.exports = (env, argv) => merge(
+  {env, argv},
+  require('wpc/required'),
+  require('wpc/argv'),
+  require('wpc/dev-server'),
 
-    {env},
+  ({context}) => ({
+    env: {
+      src: context + '/src'
+    },
+    entry: context + '/src/index.js',
+    output: {
+      path: context + '/dist'
+    },
+    devServer: {
+      host: '0.0.0.0',
+      noInfo: true
+    }
+  }),
 
-    require('wpc/resolve-src'),
-    require('wpc/global-modules'),
-    require('wpc/global-loaders'),
+  require('wpc/resolve-src'),
+  require('wpc/global-modules'),
+  require('wpc/global-loaders'),
 
-    require('wpc/define-env'),
-    require('wpc/define-version'),
+  require('wpc/define-env'),
+  require('wpc/define-version'),
 
-    require('wpc/css'),
-    require('wpc/html'),
-    require('wpc/coffee'),
-    require('wpc/json'),
-    require('wpc/yml'),
+  require('wpc/css'),
+  require('wpc/html'),
+  require('wpc/coffee'),
+  require('wpc/json'),
+  require('wpc/yml'),
 
-    require('wpc/babel'),
-    require('wpc/babel-es2015'),
-    require('wpc/babel-stage3'),
+  require('wpc/babel'),
+  require('wpc/babel-es2015'),
+  require('wpc/babel-stage3'),
 
-    require('wpc/vue'),
-    require('wpc/vue-babel'),
-    require('wpc/vue-pug'),
-    require('wpc/vue-scss'),
-    require('wpc/vue-sass'),
-    require('wpc/vue-less'),
-    require('wpc/vue-stylus'),
+  require('wpc/vue'),
+  require('wpc/vue-babel'),
 
-    require('wpc/vendor'),
-    require('wpc/manifest'),
-    require('wpc/custom'),
-    require('wpc/dump-conf'),
-    require('wpc/delete-properties')
+  require('wpc/vendor'),
+  require('wpc/manifest'),
+  require('wpc/custom'),
+  require('wpc/dump-conf'),
+  require('wpc/delete-properties')
 )
